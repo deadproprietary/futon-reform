d05302b74489ab7878fa8c7258f44a189bdad21c
deadproprietary <filipnilsson.4020@gmail.com>
2025-02-19 03:37:53 +0100

    fix ticker flicker

--- src/components/RandomTicker.svelte
+++ src/components/RandomTicker.svelte
@@ -4,20 +4,25 @@
     let pickedLine = randomLines[Math.floor( Math.random() * randomLines.length )];
     
     let i=0, isTag, text;
-    
+
+    function clearElement(elementID)
+    {
+        document.getElementById(elementID).innerHTML = "";
+    }
     function type() {
         text = pickedLine.slice(0, i++);
 
-        
-        
         document.getElementById('tickertext').innerHTML = text;
 
-        let char = text.slice(-1);
+        let char = text.slice(0);
         if( char === '<' ) isTag = true;
         if( char === '>' ) isTag = false;
 
         if (isTag) return type();
-        setTimeout(type, Math.random()*50);
+        let tickerTimeout = setTimeout(type, Math.random()*50);
+        if (text === pickedLine) {
+            clearTimeout(tickerTimeout);
+        }
     }
     type();
     
