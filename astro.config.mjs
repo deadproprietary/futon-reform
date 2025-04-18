d2eab40e11708479f1a1e712b51c2ee9b910d19e
deadproprietary <filipnilsson.4020@gmail.com>
2025-04-18 17:51:53 +0200

    misc: redirect /ocs to gdrive

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,4 +12,7 @@ export default defineConfig({
   integrations: [mdx(), svelte(), icon()],
   trailingSlash: "never",
   site: "https://yummypillow.art",
+  redirects: {
+  	"/ocs": "https://drive.google.com/drive/u/1/folders/1OyaAMihp0RRD9TQoxR4HDPAYehrL0Hbs"
+  }
 });
